ac2464e69131cd6092e882d17e37eb80892cf22c
Antoine Béland <antoine.beland@gmail.com>
2016-08-06 11:37:30 -0400

    Removed .idea folder. Added Gulp as dependency.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ![Logo](https://raw.githubusercontent.com/antoinebeland/frogger.js/master/build/assets/logo.png)
+# ![frogger.js](https://raw.githubusercontent.com/antoinebeland/frogger.js/master/build/assets/logo.png)
 Frogger game implements in TypeScript.
---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var webServer = require('gulp-webserver');
+
+gulp.task('run', function() {
+  gulp.src("build")
+    .pipe(webServer({
+      livereload: true,
+      fallback: "index.html",
+      port: 8080,
+      open: true
+    }));
+});
+
+gulp.task('default', function() {
+  // TODO
+});
